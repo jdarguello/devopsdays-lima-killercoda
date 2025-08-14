7541ef0c1686f127c5406758a556c1bd08bfcddf
Juan David Arguello <jdarguello>
2025-08-14 08:24:10 -0500

    k8s manifests

--- gitops-demo/background/backstage-setup.sh
+++ gitops-demo/background/backstage-setup.sh
@@ -6,10 +6,67 @@ export current_context=$(k config current-context)
 #Cambio al cluster local
 k config set-context kubernetes-admin@kubernetes
 
-#Pod, svc and ingress creation
-k create -f k8s/pod.yaml
-k create -f k8s/svc.yaml
-k create -f k8s/ingress.yaml
+#Pod creation
+kubectl apply -f - <<'EOF'
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    run: backstage
+  name: backstage
+spec:
+  containers:
+  - image: ghcr.io/jdarguello/cloudmanager:0.3
+    name: backstage
+    ports:
+    - name: http
+      containerPort: 7007
+    resources: {}
+  dnsPolicy: ClusterFirst
+  restartPolicy: Always
+status: {}
+EOF
+
+#Svc creation
+k create -f - << 'EOF'
+apiVersion: v1
+kind: Service
+metadata:
+  labels:
+    run: backstage
+  name: backstage
+spec:
+  ports:
+  - port: 80
+    protocol: TCP
+    targetPort: 7007
+  selector:
+    run: backstage
+status:
+  loadBalancer: {}
+EOF
+
+#Ingress creation
+k create -f - << 'EOF'
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: backstage
+spec:
+  ingressClassName: nginx
+  rules:
+  - http:
+      paths:
+      - backend:
+          service:
+            name: backstage
+            port:
+              number: 80
+        path: /
+        pathType: Prefix
+status:
+  loadBalancer: {}
+EOF
 
 #Retornar al cluster del usuario
 k config set-context $current_context

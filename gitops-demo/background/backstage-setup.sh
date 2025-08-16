e93dad573bf5ebcd32319300f63bf5286d1adacb
Juan David Arguello <jdarguello>
2025-08-16 11:28:11 -0500

    v0.10

--- gitops-demo/background/backstage-setup.sh
+++ gitops-demo/background/backstage-setup.sh
@@ -26,7 +26,7 @@ spec:
         app: backstage
     spec:
       containers:
-      - image: ghcr.io/jdarguello/cloudmanager:0.11.6
+      - image: ghcr.io/jdarguello/cloudmanager:0.10
         name: backstage
         ports:
         - name: http

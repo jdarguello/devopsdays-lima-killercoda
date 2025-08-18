431eff3dc2fbb35076acbabe7e1b4ce052f09305
Juan David Arguello <jdarguello>
2025-08-18 15:08:10 -0500

    github-user cli

--- gitops-demo/background/cli.sh
+++ gitops-demo/background/cli.sh
@@ -8,6 +8,7 @@ cd ~/CloudManager/platform-engineering/CLI/
 
 #Definir .env
 echo "GITHUB_TOKEN=$(k get secrets backstage-secrets -o jsonpath="{.data.GITHUB_TOKEN}" | base64 -d)" > .env
+echo "GITHUB_USER=$(k get secrets backstage-secrets -o jsonpath="{.data.GITHUB_USER}" | base64 -d)" >> .env
 echo "AWS_ACCESS_KEY_ID=$(k get secrets awssm-secret -o jsonpath="{.data.access-key}" | base64 -d)" >> .env
 echo "AWS_SECRET_ACCESS_KEY=$(k get secrets awssm-secret -o jsonpath="{.data.secret-access-key}" | base64 -d)" >> .env
 
